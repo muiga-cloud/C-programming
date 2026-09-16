ac9dd0da57fe856be262b7b5ebd38880cb4c9f59
muiga-cloud <muigayusuf@gmail.com>
2026-09-16 14:08:26 +0300

    Add files via upload

---
+++ CT100_30672_26 project.c
@@ -0,0 +1,21 @@
+/*
+Name : JOSEPH MUIGA WACHIRA
+Reg no: CT100/G/30672/26
+Description: Academic units offered this semester
+Date : 14/9/2026
+*/
+
+#include<stdio.h>
+int main() 
+{
+  printf("1.Fundermentals of Digital Technology and artificial inteligence. \n");
+  printf("2.introduction to computer Application. \n");
+  printf("3.Essentials of Accounting.\n");
+  printf("4.Computer system and Organization. \n");
+  printf("5.Analogue Electronics \n");
+  printf("6.Mathematics for science \n");
+  printf("7.Introduction to programming and Algorithms \n");
+  printf("8.Communication and information Literacy skills \n");
+  
+  return 0;
+}
